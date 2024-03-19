123988d41459fb5a27126da761df36adaf9431dc
TarikAZIKI <tarikaziki1@gmail.com>
2024-03-19 17:01:26 +0100

    UX Design - ajout des murs et des stickers

--- front/js/boutique.js
+++ front/js/boutique.js
@@ -1,33 +1,46 @@
-// boutique.js
-
 document.addEventListener("DOMContentLoaded", function () {
+
     const avatarsBtn = document.getElementById('avatars');
-    console.log("Hello");
     const tablesBtn = document.getElementById('tables');
-    const weaponsBtn = document.getElementById('weapons');
+    const weaponsBtn = document.getElementById('walls');
+    const dansesBtn = document.getElementById('stickers');
 
     const avatarProducts = document.getElementById('avatar-products');
     const tableProducts = document.getElementById('table-products');
-    const weaponProducts = document.getElementById('weapon-products');
+    const weaponProducts = document.getElementById('walls-products');
+    const dansesProducts = document.getElementById('stickers-products');
+
+    avatarProducts.style.display = 'flex';
+    tableProducts.style.display = 'none';
+    weaponProducts.style.display = 'none';
+    dansesProducts.style.display = 'none';
 
     avatarsBtn.addEventListener('click', () => {
         avatarProducts.style.display = 'flex';
-        avatarProducts.style.whiteSpace = nowrap;
         tableProducts.style.display = 'none';
         weaponProducts.style.display = 'none';
+        dansesProducts.style.display = 'none';
     });
-    
 
     tablesBtn.addEventListener('click', () => {
         avatarProducts.style.display = 'none';
-        tableProducts.style.display = 'block';
+        tableProducts.style.display = 'flex';
         weaponProducts.style.display = 'none';
+        dansesProducts.style.display = 'none';
     });
 
     weaponsBtn.addEventListener('click', () => {
         avatarProducts.style.display = 'none';
         tableProducts.style.display = 'none';
-        weaponProducts.style.display = 'block';
+        weaponProducts.style.display = 'flex';
+        dansesProducts.style.display = 'none';
     });
-});
 
+    dansesBtn.addEventListener('click', () => {
+        dansesProducts.style.display = 'flex';
+        avatarProducts.style.display = 'none';
+        tableProducts.style.display = 'none';
+        weaponProducts.style.display = 'none';
+    });
+    
+});
